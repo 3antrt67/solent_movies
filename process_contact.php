676048f9030695f8706ea36e5b16ca350f2f5614
Terry Antram <3antrt67@solent.ac.uk>
2018-03-16 12:39:30 +0000

    Added a contact_date field to attempt to SQL NOW() function

--- process_contact.php
+++ process_contact.php
@@ -4,7 +4,7 @@ include "database.php";
 $name = $_POST["name"];
 $message = $_POST["message"];
 
-$conn->query("INSERT INTO contact (name, message) VALUES('$name', '$message')");
+$conn->query("INSERT INTO contact (name, message, contact_date) VALUES('$name', '$message', 'NOW()')");
 
 echo "<h1>Confirmation</h1>
 		<p>Your name is $name and your message is $message.</p>
