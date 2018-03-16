9409a1a8303ac57412eafcca027900379caf8c29
Terry Antram <3antrt67@solent.ac.uk>
2018-03-16 12:46:38 +0000

    Using PHP date function into variable and attempting SQL commit

--- process_contact.php
+++ process_contact.php
@@ -3,8 +3,9 @@ include "database.php";
 
 $name = $_POST["name"];
 $message = $_POST["message"];
+$now = date("Y-m-d");
 
-$conn->query("INSERT INTO contact (name, message, contact_date) VALUES('$name', '$message', 'NOW()')");
+$conn->query("INSERT INTO contact (name, message, contact_date) VALUES('$name', '$message', '$now')");
 
 echo "<h1>Confirmation</h1>
 		<p>Your name is $name and your message is $message.</p>
