a7736f31333b87b98481d74421b14bb254a3d070
Terry Antram <3antrt67@solent.ac.uk>
2018-03-23 14:54:32 +0000

    Added JS directory with first script file

---
+++ js/modal_clear.js
@@ -0,0 +1,8 @@
+function clear_form() {
+		document.getElementById("name").value="";
+		document.getElementById("message").value="";
+	}
+	
+function show_modal(){
+	  $('#thanksModal').modal('show');
+	}
