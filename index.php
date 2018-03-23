361505571f42ed0dfc64b8bf64479e0a737d178e
Terry Antram <3antrt67@solent.ac.uk>
2018-03-23 21:44:29 +0000

    Removed the top-right close option in favour of a button that can then be used as an onclick to trigger clear function

--- index.php
+++ index.php
@@ -26,14 +26,13 @@
 <div class="modal-dialog">
 	<div class="modal-content">
 		<div class="modal-header">
-			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 			<h4 class="modal-title" id="modalLabel">Thanks for getting in contact!</h4>
 		</div>
 		<div class="modal-body">
 			<p>I will endeavour to be in touch as soon as possible!</p>
 		</div>
 		<div class="modal-footer">
-			<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			<button type="button" class="btn btn-default" data-dismiss="modal" onclick="clear_form()">Close</button>
 		</div>
 	</div>
 </div>
