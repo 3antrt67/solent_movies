e762e596e138cea44337fa3cf33fa0523b85db42
Terry Antram <3antrt67@solent.ac.uk>
2018-03-23 15:31:41 +0000

    PHP header to include on each page

---
+++ header.php
@@ -0,0 +1,16 @@
+<div id="navbar-collapse" class="collapse navbar-collapse">
+		<ul class="nav navbar-nav navbar-left">
+			<img src="images/ssu-logo.svg">
+		</ul>
+		<ul class="nav navbar-nav navbar-right">
+			<li>
+				<a class="latestRelease" href="#">New Releases</a>
+			</li>
+			<li>
+				<a class="about" href="#">About</a>
+			</li>
+			<li>
+				<a class="login" href="#">Login</a>
+			</li>
+		</ul>
+	</div>
