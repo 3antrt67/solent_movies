4bbd461edb90200d0716e5ba46439ce005f3254c
Terry Antram <3antrt67@solent.ac.uk>
2018-03-28 16:14:30 +0100

    This is the navbar that loads into index_auth upon successful login

---
+++ header_auth.php
@@ -0,0 +1,29 @@
+<head>
+	<title>Solent Movies</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src='https://www.google.com/recaptcha/api.js'></script>
+	<meta charset="UTF-8">
+	<script type="text/javascript" src="js/modal_clear.js"></script>
+</head>
+<div id="navbar-collapse" class="collapse navbar-collapse">
+		<ul class="nav navbar-nav navbar-left">
+			<img src="images/ssu-logo.svg">
+		</ul>
+		<ul class="nav navbar-nav navbar-right">
+			<li>
+				<a class="latestRelease" href="#">New Releases</a>
+			</li>
+			<li>
+				<a class="about" href="#">About</a>
+			</li>
+			<li>
+				<a class="createMovie" name="createMovie" href="#">Create Page</a>
+			</li>
+			<li>
+				<a data-toggle="modal" class="logout" data-target="#logoutModal">
+				<span class="glyphicon glyphicon-log-out"></span> Logout</a>
+			</li>
+		</ul>
+</div>
