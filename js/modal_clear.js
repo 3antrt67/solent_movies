eaf8cc04027b473e01591c1e6c8083f17b1408f1
Terry Antram <3antrt67@solent.ac.uk>
2018-04-03 19:35:55 +0100

    Changed from php form to AJAX request to facilitate returning of php get info into same page for search

--- js/modal_clear.js
+++ js/modal_clear.js
@@ -15,3 +15,28 @@ function register_modal(){
 function poster_modal(){
 		$('#createMovie').modal('show');
 }
+
+window.onload = function() {
+	document.getElementById('searchMov').addEventListener('click', searchCon);
+}
+	
+function searchCon() {
+		var search = document.getElementById("search_term");
+		var dataString = 'search_term=' + search.value;
+		if (search.value == '') {
+			alert("Please enter a film to search");
+		} else {
+		$.ajax({
+			type: "GET",
+			url: "process/search.php",
+			data: dataString,
+			cache: false,
+			success: function(response) {
+				//$("#searchResults").load(response);
+				alert(response);
+				$("#searchResults").html(response);
+				}
+			});
+		} 
+	return false;
+}
