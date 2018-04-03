9d8757c33de040bbb668c5fe4d8c4af476300626
Terry Antram <3antrt67@solent.ac.uk>
2018-04-03 19:36:32 +0100

    added head and imports into header file

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<head>
-	<title>Solent Movies</title>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<script src='https://www.google.com/recaptcha/api.js'></script>
-	<meta charset="UTF-8">
-	<script type="text/javascript" src="js/modal_clear.js"></script>
-</head>
 <?php include('header.php'); ?>
 <body>
 	<div class="container">
