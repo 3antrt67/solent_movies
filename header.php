45e7e7fd844207bdf2f3fd40a737c199323c6a34
Terry Antram <3antrt67@solent.ac.uk>
2018-04-03 19:35:19 +0100

    Changed from php form to AJAX request to facilitate returning of php get info into same page for search

--- header.php
+++ header.php
@@ -4,15 +4,23 @@ $query = "SELECT * FROM movies";
 $final = $conn->query($query);
 $counted = $final->rowCount();
 ?>
+<head>
+	<title>Solent Movies</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src='https://www.google.com/recaptcha/api.js'></script>
+	<meta charset="UTF-8">
+	<script type="text/javascript" src="js/modal_clear.js"></script>
+</head>
 <div id="navbar-collapse" class="collapse navbar-collapse">
 		<ul class="nav navbar-nav navbar-left">
 			<img src="images/ssu-logo.svg">
 		</ul>
 		<ul class="nav nabar-nav navbar-left">
-			<form action="process/search.php" method="GET">
 			<label for="search_term">Search from:</label>
-			<input name="search_term" type="text" placeholder="<?php echo $counted . " movie pages.."; ?>" />
-			<input type="submit" name="search" value="Submit" />
+			<input id="search_term" name="search_term" type="text" placeholder="<?php echo $counted . " movie pages.."; ?>" />
+			<input type="submit" id="searchMov" name="searchMov" value="Submit" />
 			</form>
 		</ul>
 		<ul class="nav navbar-nav navbar-right">
