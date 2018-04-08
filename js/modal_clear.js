c0fd009dde761dff8e9b838aa31ac5951055f61b
Terry Antram <3antrt67@solent.ac.uk>
2018-04-08 20:38:45 +0100

    Start of vue.js logic

--- js/modal_clear.js
+++ js/modal_clear.js
@@ -16,10 +16,38 @@ function poster_modal(){
 		$('#createMovie').modal('show');
 }
 
+//window.onload = function() {
+	//document.getElementById('searchMov').addEventListener('click', searchCon);
+//}
+
+function getFilm() {
+	//var film = document.getElementById("filmPage");
+	//var res = $("#filmPage").find("h2");
+	//console.log(res);
+	var res = $('.searchRes').find("h2");
+	console.log(res);
+	for (i = 0; i < res.length; i++) {
+		var fin = res[i].innerText;
+	}	
+	alert('The film is -' + fin);
+}
+
 window.onload = function() {
 	document.getElementById('searchMov').addEventListener('click', searchCon);
+	var searchResults = new Vue({
+		el: '#headTitle',
+		data: {
+		},
+		methods: {
+			select: function(event) {
+				alert(this);
+				targetId = event.currentTarget.id;
+				console.log(targetId);
+			}
+		}
+	});
 }
-	
+
 function searchCon() {
 		var search = document.getElementById("search_term");
 		var dataString = 'search_term=' + search.value;
@@ -32,7 +60,6 @@ function searchCon() {
 			data: dataString,
 			cache: false,
 			success: function(response) {
-				//$("#searchResults").load(response);
 				alert(response);
 				$("#searchResults").html(response);
 				}
