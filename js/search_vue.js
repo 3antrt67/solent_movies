44adef177cadfe65c34b730385b84ce016f18376
Terry Antram <3antrt67@solent.ac.uk>
2018-04-13 12:55:46 +0100

    This baby works. Search field input is v-model to function by which database is queried for LIKE '%searchterm%' and returns error alert on no find or results in array that are outputted in nice bootstrap card format

---
+++ js/search_vue.js
@@ -0,0 +1,77 @@
+var app = new Vue({
+	el: '#movies',
+	data:{
+		showEditModal: false,
+		errorMessage: "",
+		successMessage: "",
+		search: {keyword: ""},
+		movies: [],
+		noMovie: false,
+		clickMember: {}
+	},
+ 
+	//mounted: function(){
+		//this.getMovies();
+	//},
+ 
+	methods:{
+		searchMonitor: function() {
+			var keyword = app.toFormData(app.search);
+			axios.post('search_test.php?action=search', keyword)
+					.then(function(response) {
+						app.movies = response.data.movies;
+
+						if(response.data.movies == '') {
+							app.noMovie = true;
+						}
+						else {
+							app.noMovie = false;
+						}
+					});
+		},
+		getMovies: function(){
+			axios.get("dbo_connect.php")
+				.then(function(response){
+					console.log(response);
+					app.movies = response.data.movies;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+		},
+
+		updateFilm() {
+			var movForm = app.toFormData(app.clickMember);
+			axios.post('search_auth.php?movie=modify', movForm)
+					.then(function(response) {
+						console.log(response);
+						app.clickMember = {};
+						if(response.data.error) {
+							app.errorMessage = response.data.message;
+						}
+						else {
+							app.successMessage = response.data.message;
+						}
+					});
+		},
+
+		selectMember(member){
+			app.clickMember = member;
+		},
+		
+		toFormData: function(obj) {
+			var form_data = new FormData();
+			for(var key in obj) {
+				form_data.append(key, obj[key]);
+			}
+			return form_data;
+		},
+
+		clearMessage: function() {
+			app.errorMessage = '';
+			app.successMessage = '';
+		}
+
+
+	}
+});
