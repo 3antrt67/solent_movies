4cdc2d089f566efc6d1aa82301b9949556f48fd9
Terry Antram <3antrt67@solent.ac.uk>
2018-04-17 17:41:56 +0100

    Improved logic for login to also post to separate SQL table to input login username against time and IP address with a boolean operator of 'isloggedin' which is triggered to 1 for login

--- process/login.php
+++ process/login.php
@@ -2,6 +2,8 @@
 include "../includes/database.php";
 session_start();
 
+$now = date("Y-m-d | H:i:s");
+
 if(isset($_POST['signIn'])) {
 	$username = $_POST['usernameLogin'];
 	$password = $_POST['password'];
@@ -18,8 +20,19 @@ if(isset($_POST['signIn'])) {
 	$results = $conn->query($query);
 	if($results->rowCount() == 1) {
 		$_SESSION['username'] = $username;
-		$_SESSION['success'] = "You have successfully logged in";
-		header('location: ../index_auth.php');
+		$isloggedin = 1;
+		$IP = $_SERVER['REMOTE_ADDR'];
+		$check = "SELECT * FROM login_sessions WHERE username='$username'";
+		$exist = $conn->query($check);
+		if($exist->rowCount() == 1) {
+			$update = "UPDATE login_sessions SET IP_address='$IP', last_login='$now', current_status='$isloggedin' WHERE username='$username'";
+			$conn->query($update);
+			header('location: ../index_auth.php');
+		} else {
+			$status = "INSERT INTO login_sessions (username, IP_address, last_login, current_status) VALUES('$username', '$IP', '$now', '$isloggedin')";
+			$conn->query($status);
+			header('location: ../index_auth.php');
+		}
 	} else {
 		exit("<h1>Wrong username/password has been entered");
 	}
