0ab410052cbd2c125ba45f5923a67cd37e925412
Terry Antram <3antrt67@solent.ac.uk>
2018-04-17 17:42:30 +0100

    Improved logic for logout which now also posts to SQL table for 'isloggedin' to be set to 0 (false) to show user is no longer logged in.

--- process/logout.php
+++ process/logout.php
@@ -1,5 +1,11 @@
 <?php
+include "../includes/database.php";
 session_start();
+
+$username = $_SESSION['username'];
+$isloggedin = 0;
+$logout = "UPDATE login_sessions SET current_status='$isloggedin' WHERE username='$username'";
+$conn->query($logout);
 unset($_SESSION['username']);
 session_destroy();
 session_write_close();
