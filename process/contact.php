2cf52cfd9d59c3e2f0e4cead6f04b2ee36efa7b1
Terry Antram <3antrt67@solent.ac.uk>
2018-04-19 19:18:01 +0100

    Now directed towards an Axios post using Vue.js to post

--- process/contact.php
+++ process/contact.php
@@ -2,10 +2,25 @@
 
 include "../includes/database.php";
 
-$name = $_POST["name"];
-$message = $_POST["message"];
-$now = date("Y-m-d | h:i:s");
+$out = array('error' => false);
 
-$result = $conn->query("INSERT INTO contact (name, message, contact_date) VALUES('$name', '$message', '$now')");
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+$now = date("Y-m-d | H:i:s");
+
+$result = $conn->query("INSERT INTO contact (name, email, message, contact_date) VALUES('$name', '$email', '$message', '$now')");
+
+if($result) {
+    $out['message'] = "Your request has been received successfully";
+}
+else {
+    $out['error'] = true;
+    $out['message'] = "Could not send request";
+}
+
+header("Content-type: application/json");
+echo json_encode($out);
+die();
 
 ?>
