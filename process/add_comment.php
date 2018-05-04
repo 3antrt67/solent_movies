bc198e8ed30de4984617d13bf2b92fa79adf7403
Terry Antram <3antrt67@solent.ac.uk>
2018-05-04 13:59:24 +0100

    This includes logic for adding comment based on movie selection through vue.js form

---
+++ process/add_comment.php
@@ -0,0 +1,21 @@
+<?php
+include "../includes/database.php";
+session_start();
+
+$out = array('error' => false);
+$now = date("Y-m-d | H:i:s");
+
+$user = $_SESSION['username'];
+$content = $_POST['comment'];
+$movid = $_POST['id'];
+
+$commit = "INSERT INTO comments (username, content, timestamp, movie_id) VALUES('$user', '$content', '$now', '$movid')";
+$query = $conn->query($commit);
+
+if($query) {
+    $out['message'] = "Comment added successfully";
+}
+else {
+    $out['error'] = true;
+    $out['message'] = "Could not add comment";
+}
