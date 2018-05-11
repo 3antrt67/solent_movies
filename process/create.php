7a678df4b4451462930c58f0aa9bd22679578bbf
Terry Antram <3antrt67@solent.ac.uk>
2018-05-11 15:14:13 +0100

    Improved on logic to commit curl request to Metacritic API and pull the CriticRating field from JSON response and then commit to movie table

--- process/create.php
+++ process/create.php
@@ -19,7 +19,23 @@ if(isset($_POST['createPage'])) {
 	if($duplicate_check->rowCount() > 0) {
 		exit("<h1>Film already exists in database</h1>");
 	} else {
-		$query = "INSERT INTO movies (poster, name, director, actor, release_date, synopsis, created_by, created_time) VALUES('$poster', '$title', '$directors', '$actors', '$release', '$synopsis', '$creator', '$now')";
+		$meta = str_replace(" ", "-", $title);
+		$ratingString = strtolower($meta);
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "https://api-marcalencc-metacritic-v1.p.mashape.com/movie/" . $ratingString);
+		curl_setopt($ch, 1, CURLOPT_GET);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		$headers = [
+    		'X-Mashape-Key: 7gZBID6E0qmshW2KB4oR1PCBKJrWp1crEp9jsn78idKvabQ2A9',
+    		'Accept: application/json'
+		];
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		$output = curl_exec($ch);
+		curl_close($ch);
+		$result = json_decode($output, true);
+		$rating = ($result["0"]["Rating"]["CriticRating"]);
+		$query = "INSERT INTO movies (poster, name, director, actor, release_date, synopsis, metacritic, created_by, created_time) VALUES('$poster', '$title', '$directors', '$actors', '$release', '$synopsis', '$rating', '$creator', '$now')";
 		$conn->query($query);
 		echo("<h1>You have successfully created" . $title . "'s film page!</h1>");
 		sleep(2);
